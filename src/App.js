7470b73d81724828820269b4df54629214d2e07f
Manan Sahni <manansahni567@gmail.com>
2021-08-03 23:58:21 +0530

    Added some changes to the UI

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
   return (
     <div className={"App "+obj[0]}>
       <div className="row theme-row">
-    		<div className="col-9 name">
+    		<div className="col-8 name">
    				calc
     		</div>
 			<div className="col-2 theme">
 				<br />
 				THEME
 			</div>
-    		<div className="col-1 theme-selector">
+    		<div className="col-2 theme-selector">
 				<label for="theme-slider">1 2 3</label>
 				<br />
 				<input className={obj[0]} id="theme-slider" type="range" min="1" max="3" step="1" value={obj[1]} onChange={handleChange} />
